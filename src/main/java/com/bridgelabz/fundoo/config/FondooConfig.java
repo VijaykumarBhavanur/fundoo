2f212f0b5d812bd738c2e745e70bc821cb28c04d
Vijaykumar Bhavanur <vijaykumarbhavanur.com>
2019-12-03 10:47:54 +0530

    updated pom.xml and properties file

--- src/main/java/com/bridgelabz/fundoo/config/FondooConfig.java
+++ src/main/java/com/bridgelabz/fundoo/config/FondooConfig.java
@@ -1,15 +1,14 @@
 package com.bridgelabz.fundoo.config;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+/*import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;*/
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -19,7 +18,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-@EnableRedisRepositories
+//@EnableRedisRepositories
 public class FondooConfig {
 
 	/**
@@ -54,27 +53,24 @@ public class FondooConfig {
 				.apis(RequestHandlerSelectors.basePackage("com.bridgelabz.fundoo")).build();
 	}
 
-	@Bean
-    public JedisConnectionFactory jedisConnectionFactory() {
-        RedisProperties properties = redisProperties();
-        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
-        configuration.setHostName(properties.getHost());
-        configuration.setPort(properties.getPort());
-
-        return new JedisConnectionFactory(configuration);
-    }
-
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
-        final RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(jedisConnectionFactory());
-        template.setValueSerializer(new GenericToStringSerializer<>(Object.class));
-        return template;
-    }
-
-    @Bean
-    @Primary
-    public RedisProperties redisProperties() {
-        return new RedisProperties();
-    }
+	/*
+	 * @Bean public JedisConnectionFactory jedisConnectionFactory() {
+	 * RedisProperties properties = redisProperties(); RedisStandaloneConfiguration
+	 * configuration = new RedisStandaloneConfiguration();
+	 * configuration.setHostName(properties.getHost());
+	 * configuration.setPort(properties.getPort());
+	 * 
+	 * return new JedisConnectionFactory(configuration); }
+	 * 
+	 * @Bean public RedisTemplate<String, Object> redisTemplate() { final
+	 * RedisTemplate<String, Object> template = new RedisTemplate<>();
+	 * template.setConnectionFactory(jedisConnectionFactory());
+	 * template.setValueSerializer(new GenericToStringSerializer<>(Object.class));
+	 * return template; }
+	 * 
+	 * @Bean
+	 * 
+	 * @Primary public RedisProperties redisProperties() { return new
+	 * RedisProperties(); }
+	 */
 }
